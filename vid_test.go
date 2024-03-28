84cfe2a001c21ee038273adf5d2b184b17a35036
Loh Siu Yin <siuyin@beyondbroadcast.com>
2024-03-28 12:43:26 +0800

    fix: bug with time format in Capture

--- vid_test.go
+++ vid_test.go
@@ -6,14 +6,15 @@ import (
 )
 
 func TestCapture(t *testing.T) {
-	t.Skip("Skipping test. Uncomment to run test")
-	output := "img-loc123-" + time.Now().Format("20060101-150405")
+	//t.Skip("Skipping test. Uncomment to run test")
+	output := "img-loc123-" + time.Now().Format("20060102-150405")
 	if err := Capture(output); err != nil {
 		t.Error(err)
 	}
 }
 
 func TestFrames(t *testing.T) {
+	//t.Skip("Skipping test. Uncomment to run test")
 	output := "vid-loc123-" + time.Now().Format("20060102-150405")
 	if err := Frames(output, 50); err != nil {
 		t.Error(err)
